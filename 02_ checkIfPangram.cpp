3a93b7cd129ffdd00170420c03a9beca9ad95f56
MeghaNamdeo <96281329+MeghaNamdeo@users.noreply.github.com>
2024-11-13 21:28:42 +0530

    Create 02_ checkIfPangram.cpp

---
+++ 02_ checkIfPangram.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool checkIfPangram(string s) {
+        vector<char>alpha(26,0);
+        for(int i=0;i<s.length();i++)
+        {
+            alpha[s[i]-'a']=1;
+        }
+        for(auto i:alpha)
+        {
+            if(i==0)return false;
+        }
+        return true;
+    }
+};
