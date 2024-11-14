11dc69c20825cdd28d1a969f7670b122d114de74
MeghaNamdeo <96281329+MeghaNamdeo@users.noreply.github.com>
2024-11-14 12:44:54 +0530

    Update 03_sortString.cpp

--- 03_sortString.cpp
+++ 03_sortString.cpp
@@ -5,3 +5,30 @@ string sort(string s){
 }
 TC:O(nlogn)
   SC:O(1)
+
+
+  -------------------------------------*********************************---------------------------------------------------
+  //using count
+string sort(string s){
+     vector<int> alpha(26, 0);
+    string ans = "";
+
+    // Count frequency of lowercase alphabet characters
+    for (int i = 0; i < s.size(); i++) {
+        
+            alpha[s[i] - 'a']++;
+        
+    }
+
+    // Build the sorted string
+    for (int i = 0; i < 26; i++) {
+        char ch = 'a' + i;
+        while (alpha[i]) {
+            ans = ans + ch;
+            alpha[i]--;
+        }
+    }
+
+    return ans;
+}
+//TC and SC:O(n)
